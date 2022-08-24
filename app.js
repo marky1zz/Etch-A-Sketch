b0cdbfa1ff2a0cac2f5aff5c665a828bf3c6aaf9
Martinas Menčiūnas <mart1z5916@gmail.com>
2022-08-24 03:32:35 +0300

    finished the project

--- app.js
+++ app.js
@@ -1,11 +1,52 @@
-console.log("hello");
-const board = document.querySelector(".board");
-board.style.gridTemplateColumns = `repeat(16 , 1fr)`
-board.style.gridTemplateRows = `repeat(16 , 1fr)`
+let color = "black";
+let click = false;
 
-for (let i = 0; i < 256; i++) 
+populate = (size) =>
 {
-    const cube = document.createElement("div");
-    cube.style.border = "solid 1px";
+  let board = document.querySelector(".board");
+  let cube = board.querySelectorAll("div");
+  cube.forEach((div) => div.remove());
+  board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+
+  let amount = size * size;
+  for (let i = 0; i < amount; i++) {
+    let cube = document.createElement("div");
+    cube.addEventListener("mouseover", colorSquare);
+    cube.style.border = "groove 1px";
     board.insertAdjacentElement("beforeend", cube);
+  }
 }
+
+populate(16);
+setSize = (input) =>
+{
+    populate(input);
+}
+
+function changeColor(choice)
+{
+  color = choice;
+}
+
+function reset()
+{
+  let board = document.querySelector(".board");
+  let cube = board.querySelectorAll("div");
+  cube.forEach((div) => (div.style.backgroundColor = "white"));
+}
+
+function colorSquare() 
+{
+    if (click) {
+      if (color === "random") {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+      } else {
+        this.style.backgroundColor = color;
+      }
+    }
+  }
+
+document.querySelector(".board").addEventListener("click", () => click = !click);
+
+
